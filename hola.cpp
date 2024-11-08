5751fa1e717aa0932a7b885a7f6e4be62cc1f395
Mickel0822 <mfaragon@espe.edu.ec>
2024-11-08 08:19:58 -0500

    Version original - Agregar el hola.cpp

---
+++ hola.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Incluye la librería estándar para la entrada y salida
+using namespace std;
+
+int main() {
+    // Imprime "Hola Mundo" en la consola
+    std::cout << "Hola Mundo" << std::endl;
+    return 0;  // Indica que el programa terminó correctamente
+}
